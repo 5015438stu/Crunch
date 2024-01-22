17ff0a057488441707887f07aa428a10d5f184b0
5015438 <5015438@g.highlineschools.org>
2024-01-22 13:25:59 -0800

    ye

--- Assets/Scripts/imagechanger.cs
+++ Assets/Scripts/imagechanger.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class potraitbiggee : MonoBehaviour
+public class imagechanger : MonoBehaviour
 {
     public Image oldimage;
     public Sprite newimage;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     public void ImageChange()
     {
         oldimage.sprite = newimage;
