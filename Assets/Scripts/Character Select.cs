63d598073a8e9342a51f0d7b0752cddafa790ab9
5015438 <5015438@g.highlineschools.org>
2024-02-02 13:24:07 -0800

    0.07 - Sildes in prog
    stuff

--- Assets/Scripts/Character Select.cs
+++ Assets/Scripts/Character Select.cs
@@ -13,6 +13,9 @@ public class CharacterSelect : MonoBehaviour
     public eventsystem game;
     public int p1selected = 0;
 
+    public GameObject allbuttons;
+    public GameObject allbuttons2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,22 +35,7 @@ public class CharacterSelect : MonoBehaviour
         if (jean.selectchara == 1)
         {
             selectedcharacter = 1;
-        }
 
-        if (cinder.selectchara == 3)
-        {
-            selectedcharacter = 3;
-        }
-
-        if (biggee.selectchara == 4)
-        {
-            selectedcharacter = 4;
-        }
-
-        /// Player two
-
-        if (jean.selectchara == 1)
-        {
             if (p1selected == 2)
             {
                 selectedcharacter2 = 1;
@@ -56,6 +44,8 @@ public class CharacterSelect : MonoBehaviour
 
         if (cinder.selectchara == 3)
         {
+            selectedcharacter = 3;
+
             if (p1selected == 2)
             {
                 selectedcharacter2 = 3;
@@ -64,11 +54,23 @@ public class CharacterSelect : MonoBehaviour
 
         if (biggee.selectchara == 4)
         {
+            selectedcharacter = 4;
+
             if (p1selected == 2)
             {
                 selectedcharacter2 = 4;
             }
         }
+
+        /// Player two
+
+        if (p1selected == 1)
+        {
+            allbuttons2.SetActive(true);
+            allbuttons.SetActive(false);
+
+        }
+
     }
     public void LoadScene(string sceneName)
     {
